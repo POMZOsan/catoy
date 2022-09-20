dc9ee974cd0d2345250472b1d9f172320ef8d8d5
POMZOsan <hattori04050@gmail.com>
2022-09-20 17:14:24 +0900

    Add: Profilesコントローラーにアクション追加#35

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,26 @@
 class ProfilesController < ApplicationController
+  before_action, only: %i[ edit update ]
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @user.update(user_params)
+      redirect_to profile_path, success: t('.success')
+    else
+      flash.now[:error] = t('fail')
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :introduction)
   end
 end

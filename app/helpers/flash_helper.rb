c98dcd5680cad4c0d39672076adc5aeda72f27f7
POMZOsan <hattori04050@gmail.com>
2022-09-20 23:32:23 +0900

    Modify: flashの種類を修正

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -2,10 +2,10 @@ module FlashHelper
 
   def flash_type(msg_type)
     case msg_type
-      when 'notice' then 'alert alert-info'
+      when 'info' then 'alert alert-info'
       when 'success' then 'alert alert-success'
       when 'error' then 'alert alert-error'
-      when 'alert' then 'alert alert-alert'
+      when 'warning' then 'alert alert-warning'
     end
   end
 end

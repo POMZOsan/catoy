97488a7ddbbb7e89758526da63d4deb9d3288a32
POMZOsan <hattori04050@gmail.com>
2022-09-26 18:44:34 +0900

    Fix: レビュー一覧画面を編集

--- app/decorators/review_decorator.rb
+++ app/decorators/review_decorator.rb
@@ -7,9 +7,9 @@ class ReviewDecorator < Draper::Decorator
 
   def shop
     if object.review_block.product_type == 'Cainz'
-      "shop: カインズホーム"
+      "カインズホーム"
     elsif object.review_block.product_type == 'Rakuten'
-      "shop: 楽天24"
+      "楽天24"
     end
   end
 end

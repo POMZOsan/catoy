fede04587cd1edad8bb3a92c1f67f1f9e9eb2cca
POMZOsan <hattori04050@gmail.com>
2022-09-28 16:33:48 +0900

    Add: crud_menuを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,8 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
+
+  def mine?(object)
+    id == object.user_id
+  end
 end

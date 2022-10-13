8aedc60e48fb11121d18feefd41de25845fa7915
POMZOsan <hattori04050@gmail.com>
2022-10-13 12:26:56 +0900

    Fix: rakutenデータ整形修正

--- db/seeds/development/get_rakuten_item.rb
+++ db/seeds/development/get_rakuten_item.rb
@@ -7,7 +7,7 @@ items = [
   RakutenWebService::Ichiba::Item.search(genreId: '215364', shopCode: 'rakuten24'),
   RakutenWebService::Ichiba::Item.search(genreId: '112121', shopCode: 'rakuten24'),
 ]
-
+# けりぐるみ用にカテゴリー作成
 kerigurumi_category = Category.find(3)
 
 items.each.with_index(1) do |item, i|
@@ -23,19 +23,23 @@ items.each.with_index(1) do |item, i|
     n += 1
     item.page(n).all do |it|
       break if count > max
-
-      if it['itemName'].match?(/けりぐるみ|ケリグルミ|ケリぐるみ|蹴り/)
-        kerigurumi_category.rakutens.create(
-          name: it['itemName'],
-          image: it['mediumImageUrls'][0],
-          url: it['itemUrl']
-        )
+      # 商品名に"(個数)"が含まれ、同じ商品の個数違いが保存されてしまう為の対策
+      if item_name = it['itemName'].slice(/(.*?)(?=\()/)
+        item_name
       else
-        category.rakutens.create(
-          name: it['itemName'],
-          image: it['mediumImageUrls'][0],
-          url: it['itemUrl']
-        )
+        item_name = it['itemName']
+      end
+      # 楽天24のカテゴリーにけりぐるみがないための対策
+      if item_name.match?(/けりぐるみ|ケリグルミ|ケリぐるみ|蹴り/)
+        kerigurumi_category.rakutens.find_or_create_by(name: item_name) do |c|
+          c.image = it['mediumImageUrls'][0]
+          c.url = it['itemUrl']
+        end
+      else
+        category.rakutens.find_or_create_by(name: item_name) do |c|
+          c.image = it['mediumImageUrls'][0]
+          c.url = it['itemUrl']
+        end
       end
       count += 1
     end

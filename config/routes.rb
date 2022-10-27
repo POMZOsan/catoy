27a7846ba7f91dbb7bec9a6bcbfd6ea07d4ea814
POMZOsan <hattori04050@gmail.com>
2022-10-27 18:30:41 +0900

    Add: namespace admin追加

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,11 @@ Rails.application.routes.draw do
     get 'cainz', to: 'cainz#index'
     get 'rakuten', to: 'rakuten#index'
   end
+
+  namespace :admin do
+    root to: 'dashboards#index'
+    get 'login', to: 'user_sessions#new'
+    post 'login', to: 'user_sessions#create'
+    delete 'logout', to: 'user_sessions#destroy'
+  end
 end

bacd266ac4bb3ed2581b4a77d562175f58af6b58
POMZOsan <hattori04050@gmail.com>
2022-10-27 17:12:30 +0900

    Add: Admin用js, stylesheet設定

---
+++ app/javascript/packs/admin.js
@@ -0,0 +1,8 @@
+import Rails from "@rails/ujs"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "@fortawesome/fontawesome-free/js/all"
+import "stylesheets/admin.css"
+
+Rails.start()
+ActiveStorage.start()

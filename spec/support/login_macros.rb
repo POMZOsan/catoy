f4125908c4db3a3d74a1433ebd41928ccb1138f9
POMZOsan <hattori04050@gmail.com>
2022-10-27 12:36:01 +0900

    Fix: ログインフォームのパスワード確認を撤去

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -4,7 +4,6 @@ module LoginMacros
     click_link 'ログイン'
     fill_in 'email', with: user.email
     fill_in 'password', with: 'password'
-    fill_in 'password_confirmation', with: 'password'
     click_button 'ログイン'
   end
 end

8d7a29a1517d113bfc1a0f68efe3cdbd99649260
POMZOsan <hattori04050@gmail.com>
2022-10-29 16:59:06 +0900

    Add: レビューupdateアクション追加

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -11,6 +11,12 @@ class Admin::ReviewsController < Admin::BaseController
   def edit; end
 
   def update
+    if @review.update(review_params)
+      redirect_to admin_review_path(@review), success: t('defaults.message.update', item: Review.model_name.human)
+    else
+      flash.now[:error] = t('defaults.message.fail_update', item: Review.model_name.human)
+      render :edit
+    end
   end
 
   def detroy
@@ -28,5 +34,9 @@ class Admin::ReviewsController < Admin::BaseController
 
   def set_review
     @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:title, :content, :rate, :image)
   end
 end

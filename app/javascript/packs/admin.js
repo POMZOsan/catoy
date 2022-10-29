da837faf803e4cf8a401f832688563785263767e
POMZOsan <hattori04050@gmail.com>
2022-10-29 15:06:28 +0900

    Add: レビュー編集画面作成

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -3,6 +3,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "@fortawesome/fontawesome-free/js/all"
 import "stylesheets/admin.css"
+import "src/preview"
 
 Rails.start()
 ActiveStorage.start()

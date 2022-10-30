5d263ae509a86b8defb718d524c0d1415ed92b93
POMZOsan <hattori04050@gmail.com>
2022-10-31 00:57:32 +0900

    Add: Cainz, Rakutenにvalidation追加した為テストを修正#87

--- spec/factories/review.rb
+++ spec/factories/review.rb
@@ -9,10 +9,11 @@ FactoryBot.define do
       transient do
         sequence(:name) { |n| "cainz_nekojarashi_#{n}"}
         sequence(:url) { |n| "cainz_nekojarashi_url_#{n}"}
+        sequence(:image) { |n| "cainz_image_url_#{n}"}
       end
       after(:build) do |review, evaluator|
         nekojarashi_category = Category.find(1)
-        cainz = create(:cainz, name: evaluator.name, url: evaluator.url)
+        cainz = create(:cainz, name: evaluator.name, url: evaluator.url, image: evaluator.image)
         nekojarashi_category.cainzs << cainz
         review.save_with_product(product_id: cainz.id, product_type: "Cainz")
       end
@@ -22,10 +23,11 @@ FactoryBot.define do
       transient do
         sequence(:name) { |n| "cainz_nezumi_#{n}"}
         sequence(:url) { |n| "cainz_nezumi_url_#{n}"}
+        sequence(:image) { |n| "cainz_nezumi_image_#{n}"}
       end
       after(:build) do |review, evaluator|
         nezumi_category = Category.find(2)
-        cainz = create(:cainz, name: evaluator.name, url: evaluator.url)
+        cainz = create(:cainz, name: evaluator.name, url: evaluator.url, image: evaluator.image)
         nezumi_category.cainzs << cainz
         review.save_with_product(product_id: cainz.id, product_type: "Cainz")
       end
@@ -35,10 +37,11 @@ FactoryBot.define do
       transient do
         sequence(:name) { |n| "rakuten_kerigurumi_#{n}"}
         sequence(:url) { |n| "rakuten_kerigurumi_url_#{n}"}
+        sequence(:image) { |n| "rakuten_kerigurumi_image_#{n}"}
       end
       after(:build) do |review, evaluator|
         kerigurumi_category = Category.find(3)
-        rakuten = create(:rakuten, name: evaluator.name, url: evaluator.url)
+        rakuten = create(:rakuten, name: evaluator.name, url: evaluator.url, image: evaluator.image)
         kerigurumi_category.rakutens << rakuten
         review.save_with_product(product_id: rakuten.id, product_type: "Rakuten")
       end
@@ -48,10 +51,11 @@ FactoryBot.define do
       transient do
         sequence(:name) { |n| "rakuten_tonnel_#{n}"}
         sequence(:url) { |n| "rakuten_tonnel_url_#{n}"}
+        sequence(:image) { |n| "rakuten_tonnel_image_#{n}"}
       end
       after(:build) do |review, evaluator|
         tonnel_category = Category.find(4)
-        rakuten = create(:rakuten, name: evaluator.name, url: evaluator.url)
+        rakuten = create(:rakuten, name: evaluator.name, url: evaluator.url, image: evaluator.image)
         tonnel_category.rakutens << rakuten
         review.save_with_product(product_id: rakuten.id, product_type: "Rakuten")
       end
@@ -61,10 +65,11 @@ FactoryBot.define do
       transient do
         sequence(:name) { |n| "rakuten_other_#{n}"}
         sequence(:url) { |n| "rakuten_other_url_#{n}"}
+        sequence(:image) { |n| "rakuten_other_image_#{n}"}
       end
       after(:build) do |review, evaluator|
         other_category = Category.find(5)
-        rakuten = create(:rakuten, name: evaluator.name, url: evaluator.url)
+        rakuten = create(:rakuten, name: evaluator.name, url: evaluator.url, image: evaluator.image)
         other_category.rakutens << rakuten
         review.save_with_product(product_id: rakuten.id, product_type: "Rakuten")
       end

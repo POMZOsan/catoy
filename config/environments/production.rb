99d715639f3f5f4e6c485a0e8d57c085104a8b7c
POMZOsan <hattori04050@gmail.com>
2022-11-04 13:38:24 +0900

    Add: パスワードリセット機能#89

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,19 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default charset: 'utf-8'
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'smtp.gmail.com',
+    user_nam: Rails.application.credentials.gmail[:address],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: 'login'
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true

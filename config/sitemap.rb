1420cca9a9ad46df32d91ee9501938700772f5ad
POMZOsan <hattori04050@gmail.com>
2022-11-10 14:16:25 +0900

    Add: sitemap導入

---
+++ config/sitemap.rb
@@ -0,0 +1,15 @@
+SitemapGenerator::Sitemap.default_host = "https://www.catoy.site"
+SitemapGenerator::Sitemap.sitemaps_host = "https://s3-#{Rails.application.credentials.dig(:aws, :region)}.amazonaws.com/#{Rails.application.credentials.dig(:aws, :bucket)}"
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
+  Rails.application.credentials.dig(:aws, :bucket).to_s,
+  aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id).to_s,
+  aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key).to_s,
+  aws_region: Rails.application.credentials.dig(:aws, :region).to_s,
+)
+SitemapGenerator::Sitemap.create do
+  add reviews_path
+  add ranking_path
+  Review.find_each do |review|
+    add review_path(review), lastmod: :review.updated_at
+  end
+end

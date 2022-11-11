c31bbba278df624c5b1ad801ce228327baa18257
POMZOsan <hattori04050@gmail.com>
2022-11-11 16:31:57 +0900

    Fix: sitemap設定修正#113

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,9 +1,9 @@
 require 'aws-sdk-s3'
 
 SitemapGenerator::Sitemap.default_host = "https://www.catoy.site"
-SitemapGenerator::Sitemap.sitemaps_host = "https://s3-#{Rails.application.credentials.dig(:aws, :region)}.amazonaws.com/#{Rails.application.credentials.dig(:aws, :bucket)}"
+SitemapGenerator::Sitemap.sitemaps_host = "https://#{Rails.application.credentials.dig(:aws, :sitemap_bucket)}.s3.#{Rails.application.credentials.dig(:aws, :region)}.amazonaws.com"
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
-  Rails.application.credentials.dig(:aws, :bucket).to_s,
+  Rails.application.credentials.dig(:aws, :sitemap_bucket).to_s,
   aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id).to_s,
   aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key).to_s,
   aws_region: Rails.application.credentials.dig(:aws, :region).to_s,

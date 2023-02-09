68d004587a47a7678cdee6b2dc759ccf33432bfb
POMZOsan <hattori04050@gmail.com>
2023-02-09 15:35:49 +0900

    Add: 管理者画面レビュー検索system spec

--- spec/system/admin/review_spec.rb
+++ spec/system/admin/review_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Reviews', type: :system do
   let!(:admin) { create :user, :admin, role: 1 }
   let!(:users) { create_list :user, 4 }
-  let!(:reviews_with_cainz_nekojarashi) { create :review, :cainz_nekojarashi, user: users[0] }
-  let!(:reviews_with_cainz_nezumi) { create :review, :cainz_nezumi, user: users[1] }
-  let!(:reviews_with_rakuten_kerigurumi) { create :review, :rakuten_tonnel, user: users[2] }
-  let!(:reviews_with_rakuten_tonnel) { create :review, :rakuten_tonnel, user: users[3]}
-  let!(:reviews_with_rakuten_other) { create :review, :rakuten_other, user: admin }
+  let!(:reviews_with_cainz_nekojarashi) { create :review, :cainz_nekojarashi, user: users[0], rate: 3 }
+  let!(:reviews_with_cainz_nezumi) { create :review, :cainz_nezumi, user: users[1], rate: 5 }
+  let!(:reviews_with_rakuten_kerigurumi) { create :review, :rakuten_kerigurumi, user: users[2], rate: 3 }
+  let!(:reviews_with_rakuten_tonnel) { create :review, :rakuten_tonnel, user: users[3], rate: 1}
+  let!(:reviews_with_rakuten_other) { create :review, :rakuten_other, user: admin, rate: 4 }
 
   before { admin_login(admin) }
 
@@ -84,6 +84,65 @@ RSpec.describe 'Reviews', type: :system do
         expect(page).to have_content 'レビューを削除しました'
         reviews_after_delete = Review.count
         expect(reviews_after_delete).not_to eq reviews_before_delete
+      end
+    end
+
+    context 'search specified reviews' do
+      before do
+        click_link 'Review'
+        expect(current_path).to eq admin_reviews_path
+      end
+
+      it 'can get reviews by title or content' do
+        fill_in 'タイトル/レビュー内容', with: reviews_with_cainz_nekojarashi.title
+        find('#search-button').click
+        expect(page).to have_content reviews_with_cainz_nekojarashi.title
+        fill_in 'タイトル/レビュー内容', with: reviews_with_rakuten_tonnel.content
+        find('#search-button').click
+        expect(page).to have_content reviews_with_rakuten_tonnel.title
+      end
+
+      it 'can get reviews by rate' do
+        reviews_with_rate_3 = Review.where(rate: 3)
+        find('#q_rate').set(3)
+        find('#search-button').click
+        reviews_with_rate_3.count.times do |i|
+          expect(page).to have_content reviews_with_rate_3[i].title
+        end
+      end
+
+      it 'can get reviews by the range of created time or updated time' do
+        january_review_1 = create(:review, :rakuten_other, user: admin, rate: 3, created_at: Date.new(2023, 1, 1))
+        january_review_2 = create(:review, :cainz_nezumi, user: users[0], rate: 4, created_at: Date.new(2023, 1, 31))
+        fill_in 'q[created_at_gteq]', with: january_review_1.created_at
+        fill_in 'q[created_at_lteq]', with: january_review_2.created_at
+        find('#search-button').click
+        expect(page).to have_content january_review_1.title
+        expect(page).to have_content january_review_2.title
+      end
+
+      it 'can get reviews by toy shops' do
+        cainz_reviews = Review.shop_contain('カインズ')
+        select 'カインズ', from: 'q[shop]'
+        find('#search-button').click
+        cainz_reviews.count.times do
+          expect(page).to have_content cainz_reviews.title
+        end
+        rakuten_reviews = Review.shop_contain('楽天')
+        select '楽天', from: 'q[shop]'
+        find('#search-button').click
+        rakuten_reviews.count.times do
+          expect(page).to have_content rakuten_reviews.title
+        end
+      end
+
+      it 'can get reviews by many keywords' do
+        review = create(:review, :cainz_nezumi, user: users[2], rate: 4, created_at: Date.new(2023, 2, 1))
+        fill_in 'タイトル/レビュー内容', with: review.title
+        find('#q_rate').set(review.rate)
+        select 'カインズ', from: 'q[shop]'
+        find('#search-button').click
+        expect(page).to have_content review.title
       end
     end
   end
